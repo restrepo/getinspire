04699cdbf56919ccf9f68e3fed3baa4274087982
Diego Restrepo <rescolo@gmail.com>
2012-08-16 08:51:25 -0500

    '''Get inspire (http://inspirehep.net) records to fill the bibliography of a LaTeX file
    BibTeX is assumed as the default format of the LaTeX file. The bib file name is obtained
    from the LaTeX file and the missing BibTeX records from inspire are appended there.
    
    The program requires the \cite key in the "texkey" format of inspire (Lastname:yearcode).
    However if a \cite has an eprint number as key like hep-ph/0213456 or 1206:0001, it will
    be automatically converted to a "texkey" and both the LaTeX file and the bib file will
    be updated.
    
    The BibTeX bbl generated file can be changed by one with The LaTeX records inspire
    format (see --bbl option)
    
    The \\begin{thebibliography} format can be also processed with the output in an external
    bbl file (see -t option)

---
+++ setup.py
@@ -0,0 +1,37 @@
+""" setup.py - Script to install package using distutils
+
+For help options run:
+$ python setup.py help
+
+"""
+#Author: Ian Huston
+
+
+from setuptools import setup
+
+###############
+VERSION = "0.1.0"
+
+
+setup_args = dict(name='getinspire',
+                  version=VERSION,
+                  author='Diego Restrepo',
+                  author_email='diego.restrepo@gmail.com',
+                  url='',
+                  packages=['getinspire'],
+                  scripts=['getinspire/getinspire'],
+                  package_data={},
+                  license="Modified BSD license",
+                  description="""getinspire queries the INSPIRE HEP database and returns to fill the bibtex o bibitem records of some LaTeX file """,
+                  long_description=open('README.rst').read(),
+                  classifiers=["Topic :: Utilities",
+                               "Intended Audience :: Science/Research",
+                               "License :: OSI Approved :: BSD License",
+                               "Operating System :: OS Independent",
+                               "Programming Language :: Python",
+                               "Programming Language :: Python :: 2.6",
+                               ],
+                  )
+
+if __name__ == "__main__":
+    setup(**setup_args)
